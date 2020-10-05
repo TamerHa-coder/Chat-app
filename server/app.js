c660408952136ac1e25230046a3ac853b403991c
Tamer Hamoud <67854476+TamerHa-coder@users.noreply.github.com>
2020-10-05 15:37:05 +0300

    making sever tests work

--- server/app.js
+++ server/app.js
@@ -6,8 +6,18 @@ app.use(express.json());
 
 /* Enter code Below */
 
+const messages = [];
 
+app.get('/messages', (req, res) => {
+    res.send(messages);
+});
 
+app.post('/messages', (req, res) => {
+    const newMessage = req.body;
+    messages.push(newMessage);
+    res.send('successfully done')
+});
+    
 /* Enter code Above */
 
 module.exports = app;
