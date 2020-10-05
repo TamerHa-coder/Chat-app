8c2d64982dbfbb1c7eac670013c23363393670e5
Tamer Hamoud <67854476+TamerHa-coder@users.noreply.github.com>
2020-10-05 21:05:50 +0300

    final solution

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+function App () {
+
+  const [messages, setMessages] = useState([]);
+  const [user, setUser] = useState('');
+  const [inputValue, setInputValue] = useState('')
+
+  useEffect(()=> {
+    const randomNumber = Math.floor(Math.random() * 2000);
+    setUser("Guest#" + randomNumber)
+    setInterval( async () => {
+      const { data } = await axios.get('/messages')
+      setMessages(data)
+      console.log(data)
+    }, 1500);
+  },[])
+
+  async function postMessage (e) {
+    e.preventDefault()
+    const newMessage = {body: inputValue, user: user}
+    await axios.post('/messages', newMessage);
+    setInputValue('')
+  }
+
   return (
     <div>
-      
+      <form onSubmit={postMessage}>
+        <input 
+          id='messageInput' 
+          onChange={e => setInputValue(e.target.value)}
+        />
+        <button id="sendButton" type="submit">Send</button>
+      </form>
+      <input  id="changeUserInput" value={user} onChange={e => setUser(e.target.value)}/>
+      <div class="messageContainer">
+        {messages.map((message, i) => 
+          <div key={i} className={message.user === user ? 'my-msg msg' : 'other-msg msg'}>
+            <strong>{message.user}</strong> {message.body}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
